0e4beb1aae2ec71e31b02b001b46b58157cf00ba
baolq <baolq.work@gmail.com>
2023-09-23 18:52:24 +0700

    Update Roblox.Lua, Web.Django. Create AnhQuan, MinhDuc

--- Web.Django/Trinh-Tam-Anh/Django/mysite/myhome/views.py
+++ Web.Django/Trinh-Tam-Anh/Django/mysite/myhome/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    response = HttpResponse()
-    response.writelines('<h1> Wellcome to Django</h1>')
-    response.write('Here is app home')
-    return response
+    # response = HttpResponse()
+    # response.writelines('<h1>Welcome to Django</h1>')
+    # response.write('Here is app home')
+    # return response
+    return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
--- Web.Django/Vo-Luong-Nguyen-Hung/Django/mysite/mysite/settings.py
+++ Web.Django/Vo-Luong-Nguyen-Hung/Django/mysite/mysite/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['myhome/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME' : os.path.join(BASE_DIR, 'db.squlite3'),
     }
 }
 
@@ -116,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
